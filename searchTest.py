6e533ca1f59ae7e67ac380da25ee5a8ce2ae6ea1
Xreddr <redwardhenion@gmail.com>
2021-11-21 11:17:39 -0800

    Scanner class object for scandir function

--- searchTest.py
+++ searchTest.py
@@ -2,17 +2,26 @@
 import os
 import stat
 
-def search(target):
-    print("Searching in: ", target)
-    for entry in os.scandir(target):
-        if entry.is_file():
-            # yield entry.name
-            print(entry.name)
-        elif entry.is_dir():
-            # yield entry.name
-            print(entry.name)
-            search(entry.path)
-
+class scanner:
+    def __init__(self, target):
+        self.target = target
+        
+    def search(self):
+        print("Searching in: ", self.target)
+        for entry in os.scandir(self.target):
+            if entry.is_file():
+                # fc += 1
+                # yield entry.name
+                print(entry.name)
+            elif entry.is_dir():
+                # dc += 1
+                # yield entry.name
+                print(entry.name)
+                self.target = entry.path
+                scanner.search(self)
+        # print(fc, dc)
+        
 path = "c:/users/xreddr/repository"
 
-search(path)
+s1 = scanner(path)
+s1.search()
