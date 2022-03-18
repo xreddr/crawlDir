fbc1698c59d57d6e01951c8ec53bcb03306ba02e
xreddr <redwardhenion@gmail.com>
2022-03-18 15:39:31 -0700

    Architecture and basic function achived

--- crawl_dir/src/compare.py
+++ crawl_dir/src/compare.py
@@ -1,6 +1,4 @@
 # Compare crawled json objects!
-import os
-import datetime
 import json
 from src import search
 
@@ -12,55 +10,18 @@ def compare_crawls(saved_object):
     print(depth_criteria)
     new_string = search.scan(target_criteria, depth_criteria)
     new_object = json.loads(new_string)
-    print(type(saved_object), type(new_object))
-    print(saved_object["data"] == new_object["data"])
-    print(json.dumps(new_object, indent=2))
+    # print(type(saved_object), type(new_object))
+    # print(saved_object["data"] == new_object["data"])
+    # print(json.dumps(new_object, indent=2))
 
+    for k in saved_object["data"].keys():
+        for j in new_object["data"].keys():
+            print(k == j)
 
-# target_dir = input_object["criteria"]
-# totalFiles = 0
-# totalDir = 0
-# date = str(datetime.datetime.now())
-# data_list = []
+            del_list = [item for item in saved_object["data"]
+                        [k] if item not in new_object["data"][j]]
+            add_list = [item for item in new_object["data"]
+                        [j] if item not in saved_object["data"][k]]
 
-# for root, dirs, files in os.walk(target_dir):
-#     #print("Searching in : ", root)
-#     for directories in dirs:
-#         data_list.append(os.path.join(root, directories))
-#         #print(os.path.join(root, directories))
-#         totalDir += 1
-#     for Files in files:
-#         data_list.append(os.path.join(root, Files))
-#         #print(os.path.join(root, Files))
-#         totalFiles += 1
-#     break
-
-# data_object = {
-#     "criteria": target_dir,
-#     "paths": data_list,
-#     "files": totalFiles,
-#     "directories": totalDir,
-#     "date": date
-# }
-
-# del_list = [item for item in input_object["paths"]
-#             if item not in data_object["paths"]]
-# add_list = [item for item in data_object["paths"]
-#             if item not in input_object["paths"]]
-
-# report_object = json.dumps(data_object, indent=2)
-# print("Current content: ")
-# print(report_object)
-
-# if bool(del_list) == False:
-#     print("No items deleted.")
-# else:
-#     print("Removed items: ")
-#     print(json.dumps(del_list, indent=2))
-
-# if bool(add_list) == False:
-#     print("No items added.")
-# else:
-#     print("Added items: ")
-#     print(json.dumps(add_list, indent=2))
-# print("Since: " + input_object["date"])
+            print(del_list)
+            print(add_list)
